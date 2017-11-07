620d29e5db0fcc9d9e80c9d90c4f5b7517885ff1
Srinidhi Moodalgiri <tosto95@users.noreply.github.com>
2017-11-07 20:05:36 +0530

    Download articles/ and textrank.py

---
+++ textrank.py
@@ -0,0 +1,125 @@
+import io, nltk, itertools, os
+import networkx as nx
+
+#apply syntactic filters based on POS tags
+def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
+    return [item for item in tagged if item[1] in tags]
+
+def normalize(tagged):
+    return [(item[0].replace('.', ''), item[1]) for item in tagged]
+
+def unique_everseen(iterable, key=None):
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in itertools.ifilterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
+
+def lDistance(firstString, secondString):
+    if len(firstString) > len(secondString):
+        firstString, secondString = secondString, firstString
+    distances = range(len(firstString) + 1)
+    for index2, char2 in enumerate(secondString):
+        newDistances = [index2 + 1]
+        for index1, char1 in enumerate(firstString):
+            if char1 == char2:
+                newDistances.append(distances[index1])
+            else:
+                newDistances.append(1 + min((distances[index1], distances[index1+1], newDistances[-1])))
+        distances = newDistances
+    return distances[-1]
+
+def buildGraph(nodes):
+    gr = nx.Graph() #initialize an undirected graph
+    gr.add_nodes_from(nodes)
+    nodePairs = list(itertools.combinations(nodes, 2))
+
+    #add edges to the graph (weighted by Levenshtein distance)
+    for pair in nodePairs:
+        firstString = pair[0]
+        secondString = pair[1]
+        levDistance = lDistance(firstString, secondString)
+        gr.add_edge(firstString, secondString, weight=levDistance)
+
+    return gr
+
+def extractKeyphrases(text):
+    #tokenize the text using nltk
+    wordTokens = nltk.word_tokenize(text)
+
+    #assign POS tags to the words in the text
+    tagged = nltk.pos_tag(wordTokens)
+    textlist = [x[0] for x in tagged]
+    
+    tagged = filter_for_tags(tagged)
+    tagged = normalize(tagged)
+
+    unique_word_set = unique_everseen([x[0] for x in tagged])
+    word_set_list = list(unique_word_set)
+
+   #this will be used to determine adjacent words in order to construct keyphrases with two words
+
+    graph = buildGraph(word_set_list)
+
+    #pageRank - initial value of 1.0, error tolerance of 0,0001, 
+    calculated_page_rank = nx.pagerank(graph, weight='weight')
+
+    #most important words in ascending order of importance
+    keyphrases = sorted(calculated_page_rank, key=calculated_page_rank.get, reverse=True)
+
+    #the number of keyphrases returned will be relative to the size of the text (a third of the number of vertices)
+    aThird = len(word_set_list) / 3
+    keyphrases = keyphrases[0:aThird+1]
+
+    #take keyphrases with multiple words into consideration as done in the paper - if two words are adjacent in the text and are selected as keywords, join them
+    #together
+    modifiedKeyphrases = set([])
+    dealtWith = set([]) #keeps track of individual keywords that have been joined to form a keyphrase
+    i = 0
+    j = 1
+    while j < len(textlist):
+        firstWord = textlist[i]
+        secondWord = textlist[j]
+        if firstWord in keyphrases and secondWord in keyphrases:
+            keyphrase = firstWord + ' ' + secondWord
+            modifiedKeyphrases.add(keyphrase)
+            dealtWith.add(firstWord)
+            dealtWith.add(secondWord)
+        else:
+            if firstWord in keyphrases and firstWord not in dealtWith: 
+                modifiedKeyphrases.add(firstWord)
+
+            #if this is the last word in the text, and it is a keyword,
+            #it definitely has no chance of being a keyphrase at this point    
+            if j == len(textlist)-1 and secondWord in keyphrases and secondWord not in dealtWith:
+                modifiedKeyphrases.add(secondWord)
+        
+        i = i + 1
+        j = j + 1
+        
+    return modifiedKeyphrases
+
+def writeFiles(keyphrases, fileName):
+    "outputs the keyphrases to files"
+    print "Generating output to " + 'keywords/' + fileName
+    keyphraseFile = io.open('keywords/' + fileName, 'w+')
+    for keyphrase in keyphrases:
+        keyphraseFile.write(keyphrase + '\n')
+    keyphraseFile.close()
+
+
+#retrieve each of the articles
+articles = os.listdir("articles")
+for article in articles:
+    print 'Reading articles/' + article
+    articleFile = io.open('articles/' + article, 'r')
+    text = articleFile.read()
+    keyphrases = extractKeyphrases(text)
+    writeFiles(keyphrases, article)
